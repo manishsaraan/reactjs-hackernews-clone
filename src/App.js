40102d4297df65bbd50384119c36297e0223a86f
manishsaraan <manish021js@gmail.com>
2018-03-18 13:32:33 +0530

    Writing initial app component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const list = [
+        {
+          title: 'React',
+          url: 'https://facebook.github.io/react/',
+          author: 'Jordan Walke',
+          num_comments: 3,
+          points: 4,
+          objectID: 0,
+        },
+        {
+          title: 'Redux',
+          url: 'https://github.com/reactjs/redux',
+          author: 'Dan Abramov, Andrew Clark',
+          num_comments: 2,
+          points: 5,
+          objectID: 1,
+        },
+];
+
+const isSearched = query => item => !query || item.title.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+
 class App extends Component {
-  render() {
+  constructor(props){
+    super(props);
+    this.state = {
+      list,
+      query : ''
+    };
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+  onSearchChange(e){
+    this.setState({query : e.target.value}); 
+    
+  }
+  render() {   
+    const { query, list } = this.state; 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <form>
+        <input value={query} type="text" onChange={this.onSearchChange}/>
+      </form>     
+       {
+         list.filter(isSearched(query)).map( item =>           
+                <div key={item.objectID}>
+                  <span><a href={item.url}>{item.title}</a></span>
+                  <span>{item.author}</span>
+                  <span>{item.num_comments}</span>
+                  <span>{item.points}</span>
+                </div>            
+        )
+       }
       </div>
     );
   }
