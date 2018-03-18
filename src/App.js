b5e8ebf6bfbcccfc243b31ddf68648a069ed5fee
manishsaraan <manish021js@gmail.com>
2018-03-18 13:57:35 +0530

    Adding styles to the components

--- src/App.js
+++ src/App.js
@@ -39,22 +39,45 @@ class App extends Component {
     const { query, list } = this.state; 
     return (
       <div className="App">
-      <form>
-        <input value={query} type="text" onChange={this.onSearchChange}/>
-      </form>     
-       {
-         list.filter(isSearched(query)).map( item =>           
-                <div key={item.objectID}>
-                  <span><a href={item.url}>{item.title}</a></span>
-                  <span>{item.author}</span>
-                  <span>{item.num_comments}</span>
-                  <span>{item.points}</span>
-                </div>            
-        )
-       }
+       <div className="page">
+        <div className="interactions">
+        <Search value={query} onChange={this.onSearchChange} >
+          Search
+        </Search>
+       </div>
+        <Table list={list} pattern={query}/>   
+      </div>
       </div>
     );
   }
+}
+
+//Search bomponent
+const Search = ({value, onChange, children }) => {    
+    return (
+      <form>
+        { children }     
+        <input value={value} type="text" onChange={onChange}/>
+      </form> 
+      ); 
+}
+
+//Show list table component
+const Table = ({list, pattern}) =>  {    
+    return(
+        <div className="table">
+          {
+           list.filter(isSearched(pattern)).map( item =>           
+                  <div key={item.objectID} className="table-row">
+                    <span style={{width : '40%'}} ><a href={item.url}>{item.title}</a></span>
+                    <span style={{width : '30%'}} >{item.author}</span>
+                    <span style={{width : '15%'}} >{item.num_comments}</span>
+                    <span style={{width : '15%'}} >{item.points}</span>
+                  </div>            
+           )
+          }
+        </div>
+      );  
 }
 
 export default App;
