7b43f275d4428e5f387817d1ac8ece0852844efa
manishsaraan <manish021js@gmail.com>
2018-03-18 14:58:39 +0530

    Caching the posts with search query

--- src/App.js
+++ src/App.js
@@ -16,23 +16,35 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      result : null,
+      results : null,
       query : DEFAULT_QUERY,
-      isAjax : false
+      isAjax : false,
+      searchKey : ''
     };
     this.onSearchChange = this.onSearchChange.bind(this);
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
   }
+  
+  //check if data is present is cache or not
+  needsToSearchTopStories(query){
+    return !this.state.results[query];
+  }
+
   //update state with results from api
   setSearchTopStories(result){
     const {hits, page }  = result;
-    const oldHits  = page ===0 ? [] : this.state.result.hits;
+    const {query, searchKey} = this.state;
+    const oldHits  = page ===0 ? [] : this.state.results[searchKey].hits;
     const updateHits = [...oldHits, ...hits];
 
-    this.setState({result : {hits : updateHits, page}});
+    this.setState({
+      results : { ...this.state.results, [searchKey] : {hits : updateHits, page}}
+    });
   }
+
   //fetch data
   fetchSearchTopStories(query, page){
     let searchQuery = `${PATH_BASE}${PATH_SEARCH}${PARAM_SEARCH}${query}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`;
@@ -55,19 +67,24 @@ class App extends Component {
     
   }
 
-  onSubmit(e){
+  onSubmit(e){    
     const {query} = this.state;
-    this.fetchSearchTopStories(query, DEFAULT_PAGE);
+    this.setState({searchKey : query});
+    if(this.needsToSearchTopStories(query)){
+      this.fetchSearchTopStories(query, DEFAULT_PAGE);
+    }
     e.preventDefault();
   }
 
   componentDidMount(){
     const {query} = this.state;
+    this.setState({searchKey : query});
     this.fetchSearchTopStories(query, DEFAULT_PAGE);
   }
   render() {   
-    const { query, result, isAjax} = this.state; 
-    const page = (result && result.page) || 0;
+    const { query, results, isAjax, searchKey} = this.state; 
+    const page = (results && results[searchKey] && results[searchKey].page) || 0;
+    const list = (results && results[searchKey] && results[searchKey].hits) || [];
     return (
       <div className="App">
        <div className="page">
@@ -76,9 +93,9 @@ class App extends Component {
           Search
         </Search>
        </div>
-        { result && <Table list={result.hits}/> }  
+        {<Table list={list}/> }  
         <div className="interactions">
-          <Button onClick={ () => this.fetchSearchTopStories(query, page +1) } isAjax={isAjax}>
+          <Button onClick={ () => this.fetchSearchTopStories(searchKey, page +1) } isAjax={isAjax}>
            {isAjax === true ? "Loading...." : "More..."}
           </Button>
         </div>
